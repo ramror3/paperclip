7b0ccb194628fc5546b4961a05daa79e5052de09
ramror3 <rameshror3@gmail.com>
2017-06-29 18:12:15 +0530

    This is my first commit

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.3'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'paperclip', '~> 5.1'
+gem 'simple_form', '~> 3.5'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628070207) do
+ActiveRecord::Schema.define(version: 20170629123144) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
